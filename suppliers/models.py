03c91571c3a6c303c47eec9193140d3da6155904
Andre <andrekuratomi@gmail.com>
2022-07-18 16:25:17 -0300

    feat(suppliers/models): Model fornecedores rascunhado.

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Supplier(models.Model):
+    # id = models.CharField(max_length=255) # necessário??
+    cnpj = models.CharField(max_length=255, primary_key=True) #PK!
+    created_at = models.DateTimeField()
+    data_pagamento = models.CharField(max_length=255) # 2 em 1?
+    data_validade = models.CharField(max_length=255) # 2 em 1?
+    email = models.CharField(max_length=255)
+    franquia = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=255) # necessário?? ou dividir em first e last?
+    senha = models.CharField(max_length=255)
+    senha_provisoria = models.CharField(max_length=255)
+    status = models.BooleanField()
+    user = models.CharField(max_length=255)
+    url_dashboard = models.CharField(max_length=255) # como automatizar para o PBI fornecê-lo???
