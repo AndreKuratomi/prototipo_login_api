e15548a5ead916d0cd7b240ed708235870820062
Andre <andrekuratomi@gmail.com>
2022-07-18 17:03:50 -0300

    feat(suppliers/models): Model fornecedores atualizado itens assinatura.

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Supplier(models.Model):
-    # id = models.CharField(max_length=255) # necessário??
     cnpj = models.CharField(max_length=255, primary_key=True) #PK!
-    created_at = models.DateTimeField()
-    data_pagamento = models.CharField(max_length=255) # 2 em 1?
-    data_validade = models.CharField(max_length=255) # 2 em 1?
     email = models.CharField(max_length=255)
     franquia = models.CharField(max_length=255)
-    full_name = models.CharField(max_length=255) # necessário?? ou dividir em first e last?
-    senha = models.CharField(max_length=255)
-    senha_provisoria = models.CharField(max_length=255)
-    status = models.BooleanField()
-    user = models.CharField(max_length=255)
+    fist_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    password_provisional = models.CharField(max_length=255) # como fazer para ter duração definida??
+    signature_created_at = models.CharField(max_length=255)
+    signature_status = models.BooleanField()
+    signature_vality = models.CharField(max_length=255)
     url_dashboard = models.CharField(max_length=255) # como automatizar para o PBI fornecê-lo???
+    username = models.CharField(max_length=255)
+    username_created_at = models.DateTimeField()
