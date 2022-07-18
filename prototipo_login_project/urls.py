b92d1fab5064749b653155937de56a7d26d5da0d
Andre <andrekuratomi@gmail.com>
2022-07-18 14:59:12 -0300

    feat: django rest framework adicionado à app; refactor: django project renomeado

--- prototipo_login_project/urls.py
+++ prototipo_login_project/urls.py
@@ -1,4 +1,4 @@
-"""prototipo_login_django URL Configuration
+"""prototipo_login_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
