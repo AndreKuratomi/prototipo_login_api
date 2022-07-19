fe5807eafaffb2355a3e2b6ccae19b1ce1890515
Andre <andrekuratomi@gmail.com>
2022-07-19 10:26:52 -0300

    feat(supliers/view): primeiros rascunhos views

--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+class RegisterSupplierView(APIView):
+    def post(self, request):
+        serializer = RegisterSupplierSerializer(data=request.data)
+
+    
+    # def get(self, request):
+        # para futuramente os administradores acessarem os fornecedores via API
+
+
+class LoginSupplierView(APIView):
+    def post(self, request):
+        serializer = LoginSupplierSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.erro)
