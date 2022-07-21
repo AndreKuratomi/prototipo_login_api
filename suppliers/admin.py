f40f979341af9db0e74cde5040b228645567fda4
Andre <andrekuratomi@gmail.com>
2022-07-21 12:10:49 -0300

    feat: Rascunho para alteração de user model

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import SupplierAdmin
+from .models import Supplier
+
+admin.site.register(Supplier, SupplierAdmin)
 
 # Register your models here.
--- suppliers/models.py
+++ suppliers/models.py
@@ -1,16 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Supplier(models.Model):
-    cnpj = models.CharField(max_length=255, primary_key=True) #PK!
-    email = models.CharField(max_length=255)
+
+class Supplier(AbstractUser):
+    cnpj = models.CharField(editable=False, max_length=255, primary_key=True, unique=True) #PK!
+    email = models.CharField(editable=True, max_length=255, unique=True)
     franquia = models.CharField(max_length=255)
     fist_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    password_provisional = models.CharField(max_length=255) # como fazer para ter duração definida??
+
+    password = models.CharField(editable=True, max_length=255, unique=False)
+    password_provisional = models.CharField(max_length=255, null=True) # como fazer para ter duração definida??
+    
     signature_created_at = models.DateTimeField(max_length=255)
     signature_status = models.BooleanField()
-    signature_vality = models.CharField(max_length=255)
+    signature_vality = models.CharField(max_length=255, null=True)
+    
     url_dashboard = models.CharField(max_length=255) # como automatizar para o PBI fornecê-lo???
-    username = models.CharField(max_length=255)
+    
+    username = models.CharField(max_length=255, null=True)
     username_created_at = models.DateTimeField()
+
+
+class 
