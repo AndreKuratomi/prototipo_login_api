4474131067f75e34782d324a5290dd39b44bf415
Andre <andrekuratomi@gmail.com>
2022-07-25 12:19:52 -0300

    feat: Banco de dados trocado para PostgreSQL

--- prototipo_login_project/settings.py
+++ prototipo_login_project/settings.py
@@ -85,15 +85,15 @@ WSGI_APPLICATION = 'prototipo_login_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'usuarios_integracao_bi',
-        # 'USER': 'postgres',
-        # 'PASSWORD': '1234',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'usuarios_integracao_bi',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
