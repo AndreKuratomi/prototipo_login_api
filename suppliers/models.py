f1b83e0919911fdacdcd3273b9a07c760b7bf65c
Andre <andrekuratomi@gmail.com>
2022-07-25 10:38:43 -0300

    feat(suppliers/models): Models para login, pedido de troca de senha e troca de senha rascunhados.

--- suppliers/models.py
+++ suppliers/models.py
@@ -31,9 +31,10 @@ class CustomUserManager(BaseUserManager):
 class Supplier(AbstractUser):
     cnpj = models.CharField(editable=False, max_length=255, primary_key=True) #READ-ONLY. Talvez dê problema
     email = models.EmailField(editable=True, max_length=255, unique=True)
-    franquia = models.CharField(max_length=255, blank=True)
+    # franquia = models.CharField(max_length=255, blank=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    sexo = models.CharField(max_length=255) # COMO COLOCAR APENAS 2 OPÇÕES??
 
     password = models.CharField(editable=True, max_length=255, unique=False)
     password_provisional = models.CharField(default=uuid.uuid4, max_length=255, blank=True) # como fazer para ter duração definida??
@@ -42,7 +43,7 @@ class Supplier(AbstractUser):
     signature_status = models.BooleanField()
     signature_vality = models.CharField(max_length=255)
     
-    url_dashboard = models.CharField(max_length=255) # como automatizar para o PBI fornecê-lo???
+    url_dashboard = models.URLField(max_length=200) # como automatizar para o PBI fornecê-lo???
     
     username = models.CharField(max_length=255, null=True, unique=False)
     username_created_at = models.DateTimeField()
@@ -51,3 +52,17 @@ class Supplier(AbstractUser):
     REQUIRED_FIELDS = ['cnpj', 'first_name', 'last_name', 'password', 'signature_status', 'signature_vality', 'url_dashboard', 'username']
     objects = CustomUserManager()
 
+
+class LoginSupplier(models.Model):
+    email = models.EmailField(editable=True, max_length=255, unique=True)
+    password = models.CharField(editable=True, max_length=255, unique=False)
+
+
+class AskChangeSulpplierPassword(models.Model):
+    email = models.EmailField(editable=True, max_length=255, unique=True)
+
+
+class ChangeSuplierPassword(models.Model):
+    password_provisional = models.CharField(default=uuid.uuid4, max_length=255, blank=True) # como fazer para ter duração definida??
+    new_password = models.CharField(editable=True, max_length=255, unique=False)
+    repeat_new_password = models.CharField(editable=True, max_length=255, unique=False)
