002d36399d8edd67c30e910a7ac037ef40104f34
Andre <andrekuratomi@gmail.com>
2022-08-03 17:28:59 -0300

    feat: View rascunhada.

--- emails/serializers.py
+++ emails/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 
-class UserMail(serializers.Serializer):
+class UserMailSerializers(serializers.Serializer):
     subject = serializers.CharField(255)
     message = serializers.TextField()
     sender = serializers.EmailField(255)
     receiver = serializers.ListField(255)
 
 
-class AdminMail(serializers.serializer):
+class AdminMailSerializers(serializers.Serializer):
     subject = serializers.CharField(255)
     message = serializers.TextField()
     sender = serializers.EmailField(255)
--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from rest_framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
 
-# Create your views here.
+from emails.serializers import UserMailSerializers, AdminMailSerializers
+
+
+class UserMailView(APIView):
+    def post(self, request):
+        serializer = UserMailSerializers(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+        send_mail(request.data['subject'], request.data['message'], request.data['sender'], request.data['receiver'])
+
+        return Response({"message": "Email successfully sent"}, status=status.HTTP_200_OK)
+
+
+class AdminMailView(APIView):
+    def post(self, request):
+        serializer = AdminMailSerializers(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+        send_mail(request.data['subject'], request.data['message'], request.data['sender'], request.data['receiver'])
+
+        return Response({"message": "Email successfully sent"}, status=status.HTTP_200_OK)
