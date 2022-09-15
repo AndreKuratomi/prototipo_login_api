3c340f60727dd4d84f7933d70130442fcfc90995
Andre <andrekuratomi@gmail.com>
2022-09-15 12:44:13 -0300

    fix: Path de link AWS em CORS removido.

--- prototipo_login_project/settings.py
+++ prototipo_login_project/settings.py
@@ -160,7 +160,7 @@ AUTH_USER_MODEL = 'suppliers.Supplier'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "http://dev-bi.vestsys.com.br.s3-website-us-east-1.amazonaws.com/"
+    "http://dev-bi.vestsys.com.br.s3-website-us-east-1.amazonaws.com"
 ]
 
 CORS_ALLOW_METHODS = [
