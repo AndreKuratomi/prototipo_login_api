63e0d298c830b537f600d7cf21eb2b284ed35a8e
Andre <andrekuratomi@gmail.com>
2022-09-23 11:58:22 -0300

    feat: Atualizações supplier: patch e delete adicionados; Atualizações dashboards: View para edição favoritos criada.

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterDashboardView, DashboardByIdView, DashboardByCategoryView
+from .views import RegisterDashboardView, DashboardByIdView, DashboardEditFavoriteView, DashboardByCategoryView
 
 urlpatterns = [
     path('dashboards/', RegisterDashboardView.as_view()),
     path('dashboards/id/<dashboard_id>/', DashboardByIdView.as_view()),
+    path('dashboards/favorite/<dashboard_id>/', DashboardEditFavoriteView.as_view()),
     path('dashboards/category/<dashboard_category>/', DashboardByCategoryView.as_view()),
 ]
